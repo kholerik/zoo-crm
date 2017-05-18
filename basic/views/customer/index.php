29dd7679a57425524bb27d123f5c8ebb00964e2d
kholerik <erazers4@gmail.com>
2017-05-19 00:52:08 +0300

    fegwge

--- basic/views/customer/index.php
+++ basic/views/customer/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Покупатели';
+$this->title = 'Покупатели ';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="customer-index">
